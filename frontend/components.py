8f4967bd137dc4c45e7e0eb6dddbfdf4853e3e53
Ammar-Azman <muhammarsafuan@gmail.com>
2023-12-06 18:11:30 +0800

    feat: add chat ui

---
+++ frontend/components.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from backend.utils import (get_text_from_pdf, 
+                            get_text_chunks, 
+                            get_vectorstore, 
+                            get_conversation_chain)
+def side_bar():
+    with st.sidebar:
+        st.subheader("Your documents")
+
+        pdf_docs = st.file_uploader("Upload your pdf here 👇", 
+                                    accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                # raw text
+                raw_text:str = get_text_from_pdf(pdf_docs)
+
+                # text chunks
+                text_chunks:list = get_text_chunks(raw_text)
+
+                # vector embedding
+                vector_embedding = get_vectorstore(text_chunks)
+
+                # conversation chain
+                # initialize state
+                st.session_state.conversation = get_conversation_chain(vector_embedding)
+
+def initialize_conversation_state():
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+def handle_user_input(user_question:str):
+    """
+    Initialize state to ensure the apps 
+    remember the conversation.
+    
+    """
+    response = st.session_state.conversation({
+        "question":user_question
+    })
+    st.write(response)
--- frontend/main.py
+++ frontend/main.py
@@ -3,48 +3,26 @@ import streamlit as st
 import importlib
 import backend.utils
 importlib.reload(backend.utils)
-from backend.utils import (get_openapi_access_key, 
-                            get_text_from_pdf, 
-                            get_text_chunks, 
-                            get_vectorstore, 
-                            get_conversation_chain)
+from backend.utils import get_openapi_access_key
+from components import side_bar, initialize_conversation_state, handle_user_input
+from html_template import css, bot_template, user_template
 
 OPEN_API_KEY = get_openapi_access_key()
-
-
-
-def side_bar():
-    with st.sidebar:
-        st.subheader("Your documents")
-
-        pdf_docs = st.file_uploader("Upload your pdf here 👇", 
-                                    accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing..."):
-                # raw text
-                raw_text:str = get_text_from_pdf(pdf_docs)
-
-                # text chunks
-                text_chunks:list = get_text_chunks(raw_text)
-
-                # vector embedding
-                vector_embedding = get_vectorstore(text_chunks)
-
-                # conversation chain
-                # initialize state
-                st.session_state.conversation = get_conversation_chain(vector_embedding)
-
-def initialize_conversation_state():
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
 
 def main():
 
     st.set_page_config(page_title="Chatting with PDF yo!", 
                         page_icon="🤖")
     
+    st.write(css, unsafe_allow_html=True)
+    
     st.header("Chat with multiple PDFs 📑")
-    st.text_input("Ask any question about the file")
+    user_question = st.text_input("Ask any question about the file")
+    if user_question:
+        handle_user_input(user_question)
+
+    st.write(user_template.replace("{{MSG}}", "Hello AI!"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "Hello human!"), unsafe_allow_html=True)
 
     initialize_conversation_state()
 

dc28ee36818cfb3e315a8c5236323e7ec718a62b
Ammar-Azman <muhammarsafuan@gmail.com>
2023-12-06 14:27:34 +0800

    feat: add get_text_chunks

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,33 @@
 import streamlit as st
 
-# utils
-def get_openapi_access_key():
-    return st.secrets["OPENAPI_KEY"]
+import importlib
+import backend.utils
+importlib.reload(backend.utils)
+from backend.utils import (get_openapi_access_key, 
+                            get_text_from_pdf, 
+                            get_text_chunks, 
+                            get_vector_embedding)
+
+OPEN_API_KEY = get_openapi_access_key()
 
 
 
+def side_bar():
+    with st.sidebar:
+        st.subheader("Your documents")
+
+        pdf_docs = st.file_uploader("Upload your pdf here 👇", 
+                                    accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                # raw text
+                raw_text:str = get_text_from_pdf(pdf_docs)
+
+                # text chunks
+                text_chunks:list = get_text_chunks(raw_text)
+
+                # vector embedding
+                vector_embedding = get_vector_embedding() 
 
 
 def main():
@@ -16,13 +38,7 @@ def main():
     st.header("Chat with multiple PDFs 📑")
     st.text_input("Ask any question about the file")
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        st.file_uploader("Upload your pdf here 👇")
-        st.button("Process")
-
-    
-
+    side_bar()    
 
 if __name__ == "__main__":
     main()
